1ff4b75892f97a6ebeec6f04760a4aecb2cc2579
Suhrid-Talukder333 <suhridtalukder333@gmail.com>
2021-04-09 20:31:31 +0600

    new build

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,8 +1,20 @@
+import coverImg from '../../images/undraw_Work_time.svg'
+import './styles.css'
+import Fade from 'react-reveal/Fade';
+
 const Projects = () => {
     return (
-      <div style={{ margin: "auto", marginTop: "10rem"}}>
-        <a href="https://github.com/Suhrid-Talukder333">projects</a>
+      <>
+      <div className="cover">
+        <Fade>
+        <img className="coverImg" src={coverImg} alt="cover"></img>
+        </Fade>
       </div>
+      <div className="projects">
+        <h3>You can check out my Github Account for Projects :D</h3>
+        <h2><a href="https://github.com/Suhrid-Talukder33">GITHUB</a></h2> 
+      </div>
+      </>
     );
 }
  
